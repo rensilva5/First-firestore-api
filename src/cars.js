e4a46f14f6ab67f1f71484f082c3792bcd2860a7
Renzo Silva <renzoasilva@gmail.com>
2022-07-26 16:53:16 -0400

    New car added as practice

--- src/cars.js
+++ src/cars.js
@@ -13,3 +13,26 @@ export function getAllCars(req, res) {
       })
       .catch( err => res.status(500).send(err))
 }
+export function createCar(req, res) {
+    // get a new car from request body
+    const newCar = req.body
+    // connect to database
+    const db = dbConnect();
+    // add that car to cars collection
+    db.collection('cars').add(newCar)
+      .then(doc => {
+        res.status(201).send({
+            success: true,
+            id: doc.id
+        })
+
+      
+      })
+      .catch(err => handleError(err, res))
+    // send back new doc id
+}
+
+function handleError(err, res) {
+    console.error(err)
+    res.status(500).send(err)
+}
